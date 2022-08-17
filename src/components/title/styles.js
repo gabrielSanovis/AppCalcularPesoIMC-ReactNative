a3f80b549882fff9a7003bbe3e675bf100516b5b
gabrielSanovis <9novis9@gmail.com>
2022-08-16 21:53:36 -0300

    Criação do compoente title

---
+++ src/components/title/styles.js
@@ -0,0 +1,17 @@
+import {StyleSheet} from "react-native";
+
+const styles = StyleSheet.create({
+    boxTitle: {
+        alignItems: 'center',
+        justifyContent:'center',
+        paddingLeft:10,
+    },
+    textTitle: {
+        color: '#ff0043',
+        fontSize: 32,
+        fontWeight:'bold',
+        textTransform: 'uppercase',
+    },
+})
+
+export default styles;
